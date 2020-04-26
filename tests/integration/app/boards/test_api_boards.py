f9ee68daa4f859f0bc3a1531c76c545769f22aff
Medniy2000 <vvnikitiuk@gmail.com>
2020-04-26 11:37:55 +0300

    light fixes, boards tests v2

--- tests/integration/app/boards/test_api_boards.py
+++ tests/integration/app/boards/test_api_boards.py
@@ -83,3 +83,35 @@ def test_create_board_failed(testapp, auth_user, boards_invalid_data):
     token = auth_user["auth_data"]["tokens"]["access"]["token"]
     headers = {"Authorization": f"Bearer {token}"}
     testapp.post_json(url, boards_invalid_data, headers=headers, status=422)
+
+
+def test_update_board(testapp, func_boards, auth_user):
+    url = url_for(
+        "api_v1.update_board", board_uuid=uuid.UUID(func_boards.get_single().uuid).hex
+    )
+    token = auth_user["auth_data"]["tokens"]["access"]["token"]
+
+    headers = {"Authorization": f"Bearer {token}"}
+    data = {
+        "name": "Board 1# updated name",
+        "description": "Board #3 updated description",
+        "is_active": True,
+    }
+    resp = testapp.put_json(url, data, headers=headers, status=200)
+
+    assert resp.json["name"] == data["name"]
+    assert resp.json["description"] == data["description"]
+    assert resp.json["is_active"] == data["is_active"]
+
+
+def test_update_board_not_exists(testapp, auth_user):
+    url = url_for("api_v1.update_board", board_uuid=uuid.uuid4().hex)
+    token = auth_user["auth_data"]["tokens"]["access"]["token"]
+
+    headers = {"Authorization": f"Bearer {token}"}
+    data = {
+        "name": "Board 1# updated name",
+        "description": "Board #3 updated description",
+        "is_active": True,
+    }
+    testapp.put_json(url, data, headers=headers, status=404)
